4da4f1b891d3faf0ecda24a52d1a378110eff630
unknown <farahzayed5@gmail.com>
2025-04-23 12:45:24 +0200

    initSpring

---
+++ src/main/java/com/example/alreadytalbt/AlreadytalbtApplication.java
@@ -0,0 +1,13 @@
+package com.example.alreadytalbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlreadytalbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlreadytalbtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alreadytalbt/AlreadytalbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alreadytalbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlreadytalbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
